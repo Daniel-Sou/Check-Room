8aadc6d417911fe704619ede9780166057a3e2e3
Daniel_Sou <bb70272@umac.mo>
2019-12-06 20:34:40 +0800

    updated by Cloud Studio

--- example/client.py
+++ example/client.py
@@ -1,1 +1,24 @@
- 
+ # Import socket module 
+import socket                
+  
+# Create a socket object 
+client = socket.socket()          
+  
+# Define the port on which you want to connect 
+port = ("127.0.0.1", 30081)
+client.connect(port)
+  
+while True: 
+  # receive data from the server 
+  data = client.recv(1024)  
+  print (data.decode()) # Print the data receive
+
+  msg_input = input("Please input data >> ")
+  client.send(msg_input.encode())
+  if msg_input == 'exit':
+    break
+
+  data = client.recv(1024)
+  print(data.decode)
+  # close the connection 
+  client.close()   
--- example/server.py
+++ example/server.py
@@ -1,36 +1,46 @@
 # first of all import the socket library
 import socket
+import random
 
 # next create a socket object
-s = socket.socket()
+sk = socket.socket()
 print ("Socket successfully created")
 
 # reserve a port on your computer in our
 # case it is 12345 but it can be anything
-port = 12345
+port = 30081
 
 # Next bind to the port
 # we have not typed any ip in the ip field
 # instead we have inputted an empty string
 # this makes the server listen to requests
 # coming from other computers on the network
-s.bind(('', port))
+sk.bind(("127.0.0.1", 30081))
 print ("socket binded to %s" % (port))
 
 # put the socket into listening mode
-s.listen(5)
+sk.listen(5)
 print ("socket is listening")
 
 # a forever loop until we interrupt it or
 # an error occurs
 while True:
 
-   # Establish connection with client.
-   c, addr = s.accept()
-   print ('Got connection from', addr)
+  # Establish connection with client.
+  server, address = sk.accept()
+  print ('Got connection from', address)
 
-   # send a thank you message to the client.
-   c.send('Thank you for connecting')
+  # send a thank you message to the client.
+  server.send('Thank you for connecting')
+  msg = "Welcome to Socket World"
+  server.send(msg.encode())
+  while True:
+    data = server.recv(1024) # Receive Client message
+    print(data.decode()) # Print the data receive
+    if data == b'exit':
+      break
+    server.send(data.encode()) # handle the data from client
+    server.send(str(random.randint(1,300)).encode()) # Send the random data
 
    # Close the connection with the client
-   c.close()
+    server.close()
